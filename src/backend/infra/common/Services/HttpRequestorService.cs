85f0775ee78d8e61bea12373c273f31a127c7a00
mmarzbac <mmarzbac@its.jnj.com>
2023-06-01 09:18:26 +0300

    schema

---
+++ src/backend/infra/common/Services/HttpRequestorService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace common;
+
+
+//TODO: enforcement of some kind of schema for the calls with Newtonsoft
+public interface IHttpRequestorService
+{
+    Task<TResponse> SendRequest<TResponse>(string url, HttpMethod method, object? requestData = null);
+}
+
+public class HttpRequestorService : IHttpRequestorService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public HttpRequestorService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<TResponse> SendRequest<TResponse>(string url, HttpMethod method, object? requestData = null)
+    {
+        HttpClient client = _httpClientFactory.CreateClient();
+
+        HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+        if (requestData != null)
+        {
+            string serializedData = JsonConvert.SerializeObject(requestData);
+            request.Content = new StringContent(serializedData, System.Text.Encoding.UTF8, "application/json");
+        }
+
+        HttpResponseMessage response = await client.SendAsync(request);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseContent = await response.Content.ReadAsStringAsync();
+            TResponse result = JsonConvert.DeserializeObject<TResponse>(responseContent);
+            return result;
+        }
+
+        throw new Exception($"HTTP request failed with status code {response.StatusCode}");
+    }
+}
---
+++ src/backend/infra/common/Services/SchemaValidator.cs
@@ -0,0 +1,36 @@
+﻿namespace common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+
+public class SchemaValidator
+{
+    private readonly ConcurrentDictionary<string, JSchema> SchemaCache = new ConcurrentDictionary<string, JSchema>();
+
+ 
+
+    public JToken ParseAndValidate(string payload, string schemaPath)
+    {
+        JSchema jSchema = SchemaCache.GetOrAdd(schemaPath, LoadAndParseSchema);
+
+ 
+
+        JToken jToken = JToken.Parse(payload);
+        jToken.Validate(jSchema);
+
+ 
+
+        return jToken;
+    }
+
+ 
+
+    private JSchema LoadAndParseSchema(string schemaPath)
+    {
+        string schema = File.ReadAllText(schemaPath);
+        return JSchema.Parse(schema);
+    }
+}
