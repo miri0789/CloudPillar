7175cdbd071fdfb467fa8d2e96218ae20b97ad59
mmarzbac <mmarzbac@its.jnj.com>
2023-06-29 14:15:40 +0300

    f

---
+++ src/agent/CloudPillar.Agent/Handlers/D2CEventHandler.cs
@@ -0,0 +1,17 @@
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json.Linq;
+using shared.Entities.Messages;
+
+namespace CloudPillar.Agent.Handlers
+{
+    public interface ID2CEventHandler
+    {
+    }
+
+    public class D2CEventHandler : ID2CEventHandler
+    {
+
+
+    }
+}
