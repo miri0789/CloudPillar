b80d7137e57b162720f3807b14509850442a646a
mmarzbac <mmarzbac@its.jnj.com>
2023-07-11 12:53:41 +0300

    fix

--- src/agent/CloudPillar.Agent/Handlers/C2DSubscriptionHandler.cs
+++ src/agent/CloudPillar.Agent/Handlers/C2DSubscriptionHandler.cs
@@ -95,7 +95,7 @@ public class C2DSubscriptionHandler : IC2DSubscriptionHandler
 
         while (!cancellationToken.IsCancellationRequested)
         {
-            Message receivedMessage = null;
+            Message receivedMessage;
 
             try
             {
