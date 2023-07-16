aa92fa3efc2e8bb1263d5bcd60212244a7c09915
BatyaShindler <92884382+BatyaShindler@users.noreply.github.com>
2023-07-17 01:31:06 +0300

    clean code

--- src/shared/Shared.Logger/Logger/LoggerHandler.cs
+++ src/shared/Shared.Logger/Logger/LoggerHandler.cs
@@ -75,8 +75,6 @@ public class LoggerHandler : ILoggerHandler
         RefreshAppendersLogLevel("Debug");
         
         Log4netConfigurationValidator.ValidateConfiguration(this);
-
-        Info($"LoggerHandler constructor, app: {applicationName}");
     }
 
     public void Error(string message, params object[] args)
