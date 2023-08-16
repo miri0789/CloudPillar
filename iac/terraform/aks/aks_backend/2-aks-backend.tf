df09cc2eb2f0a41759248964aacad3050dfdcae2
Gal Moshkovitz <gmoshkov@its.jnj.com>
2023-08-16 15:18:16 +0300

    Adding TF

--- iac/terraform/aks/aks_backend/2-aks-backend.tf
+++ iac/terraform/aks/aks_backend/2-aks-backend.tf
@@ -209,6 +209,7 @@ resource "azurerm_linux_virtual_machine" "aks_backend_vm" {
     os_disk {
         caching              = "ReadWrite"
         storage_account_type = "Standard_LRS"
+        disk_size_gb = 100
     }
     /* depends_on = [azurerm_marketplace_agreement.ubuntu] */
     /* custom_data = filebase64("${path.module}/iac-vm-init.sh") */
