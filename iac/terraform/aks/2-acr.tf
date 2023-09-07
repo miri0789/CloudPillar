cf96174dff43d080eeb7b22d319116d0d93f6443
mmarzbac <mmarzbac@its.jnj.com>
2023-09-07 09:26:56 +0300

    create envirement - terraform

--- iac/terraform/aks/2-acr.tf
+++ iac/terraform/aks/2-acr.tf
@@ -1,20 +1,13 @@
 // In future there should be established also a white-acr
 resource "azurerm_container_registry" "acr"{
-  count                         = var.env == "dev" ? 1 : 0
-  name                          = "cpgreyacr"
-  resource_group_name           = "cp-rg"
-  location                      = azurerm_resource_group.aks.location
+  count                         = var.env == "dev" || var.env == "tst" ? 1 : 0 
+  name                          = "cp${var.env}acr"
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
   sku                           = "Standard"
   admin_enabled                 = false
   public_network_access_enabled = true
   network_rule_bypass_option    = "AzureServices"
   zone_redundancy_enabled       = false
   anonymous_pull_enabled        = false
-}
-
-data "azurerm_container_registry" "aks"{
-  #count                         = var.env == "dev" ? 1 : 0
-  name                          = "cpgreyacr"
-  resource_group_name           = "cp-rg"
-  depends_on = [ azurerm_container_registry.acr ]
 }
--- iac/terraform/backend/0-variables.tf
+++ iac/terraform/backend/0-variables.tf
@@ -1,5 +1,5 @@
 variable "env" {
   type        = string
   description = "The environment to assign the resources"
-  default     = "dev"
+  default     = "tst"
 }
