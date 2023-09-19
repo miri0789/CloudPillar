ec82873769ba43a47cd4c20d13493baaceebbac4
nechama <nechama_he@malam.com>
2023-09-19 15:57:19 +0300

    check large stream

--- src/agent/CloudPillar.Agent.Tests/StreamingFileUploaderHandlerTestFixture.cs
+++ src/agent/CloudPillar.Agent.Tests/StreamingFileUploaderHandlerTestFixture.cs
@@ -16,11 +16,11 @@ namespace CloudPillar.Agent.Tests
         private StreamingFileUploaderHandler _target;
 
         private const string ACTION_ID = "action123";
-        private const int MQQT_KB = 32 * 1024;
+        private const int CHUNK_SIZE = 1024;
+        private const int NUM_OF_CHUNKS = 4;
         private const string CORRELATION_ID = "abc";
         private Uri STORAGE_URI = new Uri("https://nechama.blob.core.windows.net/nechama-container");
-        private MemoryStream READ_STREAM = new MemoryStream(new byte[] { 1, 2, 3 });
-        private int totalChunks;
+
 
         [SetUp]
         public void Setup()
@@ -28,29 +28,52 @@ namespace CloudPillar.Agent.Tests
             _deviceClientMock = new Mock<IDeviceClientWrapper>();
             _d2CMessengerHandlerMock = new Mock<ID2CMessengerHandler>();
             _loggerMock = new Mock<ILoggerHandler>();
-
-            _deviceClientMock.Setup(dc => dc.GetChunkSizeByTransportType()).Returns(MQQT_KB);
+            
+            _deviceClientMock.Setup(dc => dc.GetChunkSizeByTransportType()).Returns(CHUNK_SIZE);
             _d2CMessengerHandlerMock.Setup(d => d.SendStreamingUploadChunkEventAsync(It.IsAny<byte[]>(), It.IsAny<Uri>(), It.IsAny<string>(), It.IsAny<long>())).Returns(Task.CompletedTask);
 
             _target = new StreamingFileUploaderHandler(_d2CMessengerHandlerMock.Object, _deviceClientMock.Object, _loggerMock.Object);
-
-            totalChunks = (int)Math.Ceiling((double)READ_STREAM.Length / MQQT_KB);
         }
-
         [Test]
-        public async Task SendStreamingUploadChunks_SendChunks_VerifyChunkCalls()
+        public async Task SendStreamingUploadChunks_SingleChunk_CompleteTask()
         {
-            await _target.UploadFromStreamAsync(READ_STREAM, STORAGE_URI, ACTION_ID, CORRELATION_ID, CancellationToken.None);
+            var stream = CreateLargeStream(CHUNK_SIZE * 1);
 
-            _d2CMessengerHandlerMock.Verify(h => h.SendStreamingUploadChunkEventAsync(It.IsAny<byte[]>(), STORAGE_URI, ACTION_ID, It.IsAny<long>()), Times.Exactly(totalChunks));
-        }
-
-        [Test]
-        public async Task SendStreamingUploadChunks_SendChunks_CompleteTask()
-        {
-            await _target.UploadFromStreamAsync(READ_STREAM, STORAGE_URI, ACTION_ID, CORRELATION_ID, CancellationToken.None);
+            await _target.UploadFromStreamAsync(stream, STORAGE_URI, ACTION_ID, CORRELATION_ID, CancellationToken.None);
 
             _deviceClientMock.Verify(w => w.CompleteFileUploadAsync(It.IsAny<FileUploadCompletionNotification>(), CancellationToken.None), Times.Once);
         }
+
+        [Test]
+        public async Task SendStreamingUploadChunks_LargeStream_VerifyChunkCalls()
+        {
+
+            var largeStream = CreateLargeStream(CHUNK_SIZE * NUM_OF_CHUNKS);
+            await _target.UploadFromStreamAsync(largeStream, STORAGE_URI, ACTION_ID, CORRELATION_ID, CancellationToken.None);
+
+            _d2CMessengerHandlerMock.Verify(w => w.SendStreamingUploadChunkEventAsync(It.IsAny<byte[]>(), It.IsAny<Uri>(), It.IsAny<string>(), It.IsAny<long>()), Times.Exactly(NUM_OF_CHUNKS));
+        }
+        public static Stream CreateLargeStream(long streamSize)
+        {
+            var random = new Random();
+            var stream = new MemoryStream();
+
+            // Define the chunk size
+
+            // Generate random data and write it to the stream
+            for (long i = 0; i < streamSize; i += CHUNK_SIZE)
+            {
+                var chunk = new byte[CHUNK_SIZE];
+                random.NextBytes(chunk);
+                stream.Write(chunk, 0, CHUNK_SIZE);
+            }
+
+            // Reset the stream position to the beginning
+            stream.Seek(0, SeekOrigin.Begin);
+
+            return stream;
+        }
     }
+
+
 }
