3a30d92390f46f58a6d23b1158800caa77081c18
mmarzbac <mmarzbac@its.jnj.com>
2023-10-12 10:15:14 +0300

    try

--- deploy/terraform/aks/app-config.tf
+++ deploy/terraform/aks/app-config.tf
@@ -1,8 +1,8 @@
 resource "azurerm_app_configuration" "aks" {
-  name                = "cp-${var.env}-conf"
+  name                = "cp-${var.env}-config"
   resource_group_name = azurerm_resource_group.aks.name
   location            = var.location
-  sku                 = "free"
+  sku                 = "standard"
 }
 
 
