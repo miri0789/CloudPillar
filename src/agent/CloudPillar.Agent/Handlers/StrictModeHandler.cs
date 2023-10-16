463ef44675ae37a5b31862be41a095754cc12cfe
nechama <nechama_he@malam.com>
2023-10-16 11:01:29 +0300

    change error message in strict moe

--- src/agent/CloudPillar.Agent/Handlers/StrictModeHandler.cs
+++ src/agent/CloudPillar.Agent/Handlers/StrictModeHandler.cs
@@ -11,8 +11,15 @@ public class StrictModeHandler : IStrictModeHandler
     }
     public void CheckAuthentucationMethodValue()
     {
-        if (!_appSettings.PermanentAuthentucationMethods.Equals(AuthenticationConstants.AUTHENTICATION_X509) ||
-        !_appSettings.ProvisionalAuthentucationMethods.Equals(AuthenticationConstants.AUTHENTICATION_SAS))
-            throw new InvalidOperationException("Authentucation Methods configuration is not valid");
+        if (_appSettings.StrictMode == false) { return; }
+
+        if (!_appSettings.PermanentAuthentucationMethods.Equals(AuthenticationConstants.AUTHENTICATION_X509))
+        {
+            throw new InvalidOperationException($"PermanentAuthentucationMethods value in appSettings.json must be X509, The value {_appSettings.PermanentAuthentucationMethods} is not valid");
+        }
+        if (!_appSettings.ProvisionalAuthentucationMethods.Equals(AuthenticationConstants.AUTHENTICATION_SAS))
+        {
+            throw new InvalidOperationException($"ProvisionalAuthentucationMethods value in appSettings.json must be SAS, The value {_appSettings.ProvisionalAuthentucationMethods} is not valid");
+        }
     }
 }
