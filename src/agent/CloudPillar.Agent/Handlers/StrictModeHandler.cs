227421eb385ee985389ff7477e55b82b0efe8a4e
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-10-24 15:36:06 +0300

    remove uncessary code

--- src/agent/CloudPillar.Agent/Handlers/StrictModeHandler.cs
+++ src/agent/CloudPillar.Agent/Handlers/StrictModeHandler.cs
@@ -132,11 +132,6 @@ public class StrictModeHandler : IStrictModeHandler
     private FileRestrictionDetails GetRestrinctionsByZone(string fileName, TwinActionType actionType)
     {
         List<FileRestrictionDetails> actionRestrictions = GetRestrictionsByActionType(actionType);
-        // FileRestrictionDetails zoneRestrictions = actionRestrictions.FirstOrDefault(x => fileName.Contains(x.Root));
-        // string[] array = { "c:/demo/", "c:/demo/images/" };
-        // string targetString = "c:/demo/images/sub/image1.jpg";
-
-        // var bestMatch = actionRestrictions.OrderBy(s => ComputeLevenshteinDistance(s.Root, fileName)).First();
         
         actionRestrictions = actionRestrictions.Where(x=>fileName.Contains(x.Root)).ToList();
         var bestMatch = actionRestrictions
@@ -144,48 +139,7 @@ public class StrictModeHandler : IStrictModeHandler
                    .FirstOrDefault();
         return bestMatch;
     }
-    private int ComputeLevenshteinDistance(string s, string t)
-    {
-        if (string.IsNullOrEmpty(s))
-        {
-            return string.IsNullOrEmpty(t) ? 0 : t.Length;
-        }
-
-        if (string.IsNullOrEmpty(t))
-        {
-            return s.Length;
-        }
-
-        int[,] d = new int[s.Length + 1, t.Length + 1];
-
-        for (int i = 0; i <= s.Length; i++)
-        {
-            d[i, 0] = i;
-        }
-
-        for (int j = 0; j <= t.Length; j++)
-        {
-            d[0, j] = j;
-        }
-
-        for (int j = 1; j <= t.Length; j++)
-        {
-            for (int i = 1; i <= s.Length; i++)
-            {
-                if (s[i - 1] == t[j - 1])
-                {
-                    d[i, j] = d[i - 1, j - 1];
-                }
-                else
-                {
-                    d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + 1);
-                }
-            }
-        }
-
-        return d[s.Length, t.Length];
-    }
-
+ 
     private List<string> GetAllowRestrictions(FileRestrictionDetails zoneRestrictions)
     {
         List<string> allowPatterns = zoneRestrictions?.AllowPatterns ?? new List<string>();
