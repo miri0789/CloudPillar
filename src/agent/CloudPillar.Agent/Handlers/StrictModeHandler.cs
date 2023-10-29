cc59cbd2f348f363dc636fcf735470ba8447e4a2
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-10-29 11:46:45 +0200

    fix pr comment

--- src/agent/CloudPillar.Agent/Handlers/StrictModeHandler.cs
+++ src/agent/CloudPillar.Agent/Handlers/StrictModeHandler.cs
@@ -117,14 +117,14 @@ public class StrictModeHandler : IStrictModeHandler
     {
         _logger.Info($"Get restrictions for {actionType} action");
 
-        switch (actionType)
+        if (actionType == TwinActionType.SingularDownload)
         {
-            case TwinActionType.SingularDownload:
-                return _appSettings.FilesRestrictions.Where(x => x.Type == StrictModeAction.Dwonload.ToString()).ToList();
-            case TwinActionType.SingularUpload:
-                return _appSettings.FilesRestrictions.Where(x => x.Type == StrictModeAction.Upload.ToString()).ToList();
-            default: return _appSettings.FilesRestrictions;
+            return _appSettings.FilesRestrictions.Where(x => x.Type == StrictModeAction.Dwonload.ToString()).ToList();
         }
+        else
+        {
+            return _appSettings.FilesRestrictions.Where(x => x.Type == StrictModeAction.Upload.ToString()).ToList();
+        }       
     }
 
     private FileRestrictionDetails GetRestrinctionsByZone(string fileName, TwinActionType actionType)
