e1eb0b17cc7c5cd1baa8a0b8697b9612723e04cf
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-11-07 10:38:34 +0200

    remove await

--- src/agent/CloudPillar.Agent/Controllers/AgentController.cs
+++ src/agent/CloudPillar.Agent/Controllers/AgentController.cs
@@ -105,7 +105,7 @@ public class AgentController : ControllerBase
     [HttpPost("SetReady")]
     public async Task<ActionResult<string>> SetReadyAsync()
     {
-        await _stateMachineHandler.SetStateAsync(DeviceStateType.Ready);
+        _stateMachineHandler.SetStateAsync(DeviceStateType.Ready);
         return await _twinHandler.GetTwinJsonAsync();
     }
 
@@ -123,7 +123,7 @@ public class AgentController : ControllerBase
         var deviceId = HttpContext.Request.Headers[Constants.X_DEVICE_ID].ToString();
         var secretKey = HttpContext.Request.Headers[Constants.X_SECRET_KEY].ToString();
         await _symmetricKeyProvisioningHandler.ProvisioningAsync(deviceId, cancellationToken);
-        await _stateMachineHandler.SetStateAsync(DeviceStateType.Provisioning);
+        _stateMachineHandler.SetStateAsync(DeviceStateType.Provisioning);
         await _twinHandler.UpdateDeviceSecretKeyAsync(secretKey);
     }
 }
