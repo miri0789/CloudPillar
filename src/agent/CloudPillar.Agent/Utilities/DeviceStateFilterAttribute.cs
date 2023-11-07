cf389cc6f67558604572ced299fde576ffec6527
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-11-07 10:25:11 +0200

    AttributeTargets.Class to DeviceStateFilterAttribute

--- src/agent/CloudPillar.Agent/Utilities/DeviceStateFilterAttribute.cs
+++ src/agent/CloudPillar.Agent/Utilities/DeviceStateFilterAttribute.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Shared.Entities.Twin;
 
-[AttributeUsage(AttributeTargets.Method)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class DeviceStateFilterAttribute : ActionFilterAttribute
 {
     public override void OnActionExecuting(ActionExecutingContext context)
