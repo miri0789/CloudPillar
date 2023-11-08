1b62faa23dc9efcfbc6da5ad65093ec7a699a7e0
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-11-08 12:18:09 +0200

    unit test to agent controller and device state filter attribute

---
+++ src/agent/UnitTests/AgentControllerTestFixture.cs
@@ -0,0 +1,46 @@
+using CloudPillar.Agent.Controllers;
+using CloudPillar.Agent.Entities;
+using CloudPillar.Agent.Handlers;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Moq;
+using Shared.Entities.Twin;
+using Shared.Logger;
+
+namespace CloudPillar.Agent.Tests
+{
+    [TestFixture]
+    public class AgentControllerTestFixture
+    {
+        private Mock<ITwinHandler> _twinHandler;
+        private Mock<IValidator<UpdateReportedProps>> _updateReportedPropsValidator;
+        private Mock<IDPSProvisioningDeviceClientHandler> _dPSProvisioningDeviceClientHandler;
+        private Mock<ISymmetricKeyProvisioningHandler> _symmetricKeyProvisioningHandler;
+        private Mock<IValidator<TwinDesired>> _twinDesiredPropsValidator;
+        private Mock<IStateMachineHandler> _stateMachineHandler;
+        private Mock<ILoggerHandler> _loggerMock;
+        private ActionExecutingContext context;
+        private AgentController _target;
+        public AgentControllerTestFixture()
+        {
+
+            _twinHandler = new Mock<ITwinHandler>();
+            _updateReportedPropsValidator = new Mock<IValidator<UpdateReportedProps>>();
+            _dPSProvisioningDeviceClientHandler = new Mock<IDPSProvisioningDeviceClientHandler>();
+            _symmetricKeyProvisioningHandler = new Mock<ISymmetricKeyProvisioningHandler>();
+            _twinDesiredPropsValidator = new Mock<IValidator<TwinDesired>>();
+            _stateMachineHandler = new Mock<IStateMachineHandler>();
+            _loggerMock = new Mock<ILoggerHandler>();
+
+            _target = new AgentController(_twinHandler.Object, _updateReportedPropsValidator.Object, _dPSProvisioningDeviceClientHandler.Object,
+                        _symmetricKeyProvisioningHandler.Object, _twinDesiredPropsValidator.Object, _stateMachineHandler.Object, _loggerMock.Object);
+        }
+
+        [Test]
+        public async Task SetBusyAsync_ValidProccess_Success()
+        {
+            await _target.SetBusyAsync();
+            _twinHandler.Verify(h => h.GetLatestTwinAsync(CancellationToken.None), Times.Once);
+        }
+    }
+}
