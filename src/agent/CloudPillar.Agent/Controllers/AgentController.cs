4329a2c60fe772c52747090da790d9a9ba0ffe20
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-11-08 10:22:58 +0200

    remove unseccary using

--- src/agent/CloudPillar.Agent/Controllers/AgentController.cs
+++ src/agent/CloudPillar.Agent/Controllers/AgentController.cs
@@ -1,11 +1,9 @@
 
 using CloudPillar.Agent.Entities;
 using CloudPillar.Agent.Handlers;
-using CloudPillar.Agent.Wrappers;
 using FluentValidation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.FileSystemGlobbing;
 using Shared.Entities.Twin;
 using Shared.Logger;
 
