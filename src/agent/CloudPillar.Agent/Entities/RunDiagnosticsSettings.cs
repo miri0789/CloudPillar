2f26ce7c00b98c172cb83567692b008d2d79c6dd
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-11-12 14:24:44 +0200

    Task 11990: Creation of a small file with random content and Upload BY streaming

---
+++ src/agent/CloudPillar.Agent/Entities/RunDiagnosticsSettings.cs
@@ -0,0 +1,4 @@
+public class RunDiagnosticsSettings
+{
+    public int FleSizeKB { get; set; } = Constants.DIAGNOSTICS_FILE_SIZE_KB;
+}
---
+++ src/agent/CloudPillar.Agent/Handlers/RunDiagnosticsHandler.cs
@@ -0,0 +1,68 @@
+using CloudPillar.Agent.Entities;
+using Microsoft.Extensions.Options;
+using Shared.Entities.Twin;
+using Shared.Logger;
+
+namespace CloudPillar.Agent.Handlers;
+public class RunDiagnosticsHandler : IRunDiagnosticsHandler
+{
+    private const int BYTE_SIZE = 1024;
+    private const string FILE_NAME = "diagnosticFile";
+    private const string FILE_EXSTENSION = ".txt";
+    private string basePath = AppDomain.CurrentDomain.BaseDirectory;
+    private string destPath;
+
+    private readonly IFileUploaderHandler _fileUploaderHandler;
+    private readonly RunDiagnosticsSettings _runDiagnosticsSettings;
+    private readonly ILoggerHandler _logger;
+
+    public RunDiagnosticsHandler(IFileUploaderHandler fileUploaderHandler, IOptions<RunDiagnosticsSettings> runDiagnosticsSettings, ILoggerHandler logger)
+    {
+        _fileUploaderHandler = fileUploaderHandler ?? throw new ArgumentNullException(nameof(fileUploaderHandler));
+        _runDiagnosticsSettings = runDiagnosticsSettings?.Value ?? throw new ArgumentNullException(nameof(runDiagnosticsSettings));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        destPath = Path.Combine(basePath, FILE_NAME + FILE_EXSTENSION);
+    }
+
+    public async Task CreateFileAsync()
+    {
+        var fileSize = _runDiagnosticsSettings.FleSizeKB * BYTE_SIZE;
+        try
+        {
+            //create random content
+            var bytes = new Byte[fileSize];
+            new Random().NextBytes(bytes);
+
+            using (FileStream fileStream = new FileStream(destPath, FileMode.Create))
+            {
+                fileStream.SetLength(fileSize);
+                await fileStream.WriteAsync(bytes);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"error during creating a file {ex.Message}");
+            throw ex;
+        }
+    }
+
+    public async Task UploadFileAsync(CancellationToken cancellationToken)
+    {
+        var uploadAction = new UploadAction()
+        {
+            Action = TwinActionType.SingularUpload,
+            Description = "upload file by run diagnostic",
+            ActionId = Guid.NewGuid().ToString(),
+            Method = FileUploadMethod.Stream,
+            FileName = destPath
+        };
+
+        var actionToReport = new ActionToReport();
+        await _fileUploaderHandler.UploadFilesToBlobStorageAsync(destPath, uploadAction, actionToReport, cancellationToken);
+    }
+
+
+    public async Task DownloadFile(CancellationToken cancellationToken)
+    {
+    }
+}
