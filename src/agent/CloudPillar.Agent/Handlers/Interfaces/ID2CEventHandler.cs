c2167454b84bc10e94aef1e703b8b9890027bdbf
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-11-14 15:51:49 +0200

    changes

--- src/agent/CloudPillar.Agent/Handlers/Interfaces/ID2CEventHandler.cs
+++ src/agent/CloudPillar.Agent/Handlers/Interfaces/ID2CEventHandler.cs
@@ -4,7 +4,7 @@ using System.Security.Cryptography.X509Certificates;
 namespace CloudPillar.Agent.Handlers;
 public interface ID2CMessengerHandler
 {
-    Task SendFirmwareUpdateEventAsync(string fileName, string actionId , long? startPosition = null, long? endPosition = null);
+    Task SendFirmwareUpdateEventAsync(string fileName, string actionId, long? startPosition = null, long? endPosition = null);
     Task SendStreamingUploadChunkEventAsync(byte[] buffer, Uri storageUri, string actionId, long currentPosition, string checkSum, bool fromRunDiagnostic);
     Task ProvisionDeviceCertificateEventAsync(X509Certificate2 certificate);
 }
