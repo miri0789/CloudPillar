1cbc19d8160c9d703f0d7d8946cf555c56cb266e
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-11-19 14:37:45 +0200

    update terraform

--- deploy/terraform/iot/3-storage.tf
+++ deploy/terraform/iot/3-storage.tf
@@ -12,3 +12,28 @@ resource "azurerm_storage_container" "iot" {
   storage_account_name  = azurerm_storage_account.iot.name
   container_access_type = "private"
 }
+
+resource "azurerm_storage_container" "diagnostics" {
+  name                  = "diagnostics-container"
+  storage_account_name  = azurerm_storage_account.iot.name
+  container_access_type = "private"
+}
+
+
+resource "azurerm_storage_management_policy" "diagnostics-policy" {
+  storage_account_id = azurerm_storage_account.iot.id
+
+  rule {
+    name    = "delete-after-one-day"
+    enabled = true
+    filters {
+      prefix_match = [azurerm_storage_container.diagnostics.name]
+      blob_types   = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        delete_after_days_since_creation_greater_than = 1
+      }
+    }
+  }
+}
