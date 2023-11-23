d2c625c8b7408b25e81501aa84dcea2b4a3c728d
mmarzbac <mmarzbac@its.jnj.com>
2023-11-23 15:30:14 +0200

    fix blob streamer

--- src/backend/Infra/Common/Services/DeviceClientWrapper.cs
+++ src/backend/Infra/Common/Services/DeviceClientWrapper.cs
@@ -8,27 +8,30 @@ public class DeviceClientWrapper : IDeviceClientWrapper
         var serviceClient = ServiceClient.CreateFromConnectionString(connString);
         return serviceClient;
     }
-    public async Task SendAsync(ServiceClient _serviceClient, string deviceId, Message c2dMessage)
+    public async Task SendAsync(ServiceClient serviceClient, string deviceId, Message c2dMessage)
     {
-        while (true)
+        using (var localServiceClient = serviceClient)
         {
-            try
+            while (true)
             {
-                await _serviceClient.SendAsync(deviceId, c2dMessage);
-                break; // Succeeded
-            }
-            catch (Exception ex)
-            {
-                const string queueLimitMsg = "C2D messages enqueued for the device exceeded the queue limit";
-                const string throttlingBacklogMsg = "Throttling backlog element expired";
-                if (ex.Message.Contains(queueLimitMsg) || ex.Message.Contains(throttlingBacklogMsg))
+                try
                 {
-                    Console.WriteLine($"Overflow of 50 messages in the C2D queue, stalling until client '{deviceId}' unloads some.");
-                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    await localServiceClient.SendAsync(deviceId, c2dMessage);
+                    break; // Succeeded
                 }
-                else
+                catch (Exception ex)
                 {
-                    throw ex;
+                    const string queueLimitMsg = "C2D messages enqueued for the device exceeded the queue limit";
+                    const string throttlingBacklogMsg = "Throttling backlog element expired";
+                    if (ex.Message.Contains(queueLimitMsg) || ex.Message.Contains(throttlingBacklogMsg))
+                    {
+                        Console.WriteLine($"Overflow of 50 messages in the C2D queue, stalling until client '{deviceId}' unloads some.");
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                    }
+                    else
+                    {
+                        throw ex;
+                    }
                 }
             }
         }
