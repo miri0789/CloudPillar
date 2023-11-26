46f31659d1c4f16efc49bfb578ac63277ac3fe62
mmarzbac <mmarzbac@its.jnj.com>
2023-11-26 13:56:17 +0200

    fix

--- src/backend/Infra/Common/Services/DeviceClientWrapper.cs
+++ src/backend/Infra/Common/Services/DeviceClientWrapper.cs
@@ -10,28 +10,25 @@ public class DeviceClientWrapper : IDeviceClientWrapper
     }
     public async Task SendAsync(ServiceClient serviceClient, string deviceId, Message c2dMessage)
     {
-        using (var localServiceClient = serviceClient)
+        while (true)
         {
-            while (true)
+            try
             {
-                try
+                await serviceClient.SendAsync(deviceId, c2dMessage);
+                break;
+            }
+            catch (Exception ex)
+            {
+                const string queueLimitMsg = "C2D messages enqueued for the device exceeded the queue limit";
+                const string throttlingBacklogMsg = "Throttling backlog element expired";
+                if (ex.Message.Contains(queueLimitMsg) || ex.Message.Contains(throttlingBacklogMsg))
                 {
-                    await localServiceClient.SendAsync(deviceId, c2dMessage);
-                    break;
+                    Console.WriteLine($"Overflow of 50 messages in the C2D queue, stalling until client '{deviceId}' unloads some.");
+                    await Task.Delay(TimeSpan.FromSeconds(1));
                 }
-                catch (Exception ex)
+                else
                 {
-                    const string queueLimitMsg = "C2D messages enqueued for the device exceeded the queue limit";
-                    const string throttlingBacklogMsg = "Throttling backlog element expired";
-                    if (ex.Message.Contains(queueLimitMsg) || ex.Message.Contains(throttlingBacklogMsg))
-                    {
-                        Console.WriteLine($"Overflow of 50 messages in the C2D queue, stalling until client '{deviceId}' unloads some.");
-                        await Task.Delay(TimeSpan.FromSeconds(1));
-                    }
-                    else
-                    {
-                        throw ex;
-                    }
+                    throw ex;
                 }
             }
         }
