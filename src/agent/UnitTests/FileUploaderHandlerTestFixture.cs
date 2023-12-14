42f6c055a23196a2f8558183495766598aabfa49
mmarzbac <mmarzbac@its.jnj.com>
2023-12-14 14:17:24 +0200

    fix

--- src/agent/UnitTests/FileUploaderHandlerTestFixture.cs
+++ src/agent/UnitTests/FileUploaderHandlerTestFixture.cs
@@ -44,7 +44,7 @@ public class FileUploaderHandlerTestFixture
         _deviceClientWrapperMock.Setup(device => device.GetFileUploadSasUriAsync(It.IsAny<FileUploadSasUriRequest>(), It.IsAny<CancellationToken>()))
         .ReturnsAsync(sasUriResponse);
 
-        _deviceClientWrapperMock.Setup(device => device.GetBlobUriAsync(It.IsAny<FileUploadSasUriResponse>(), It.IsAny<CancellationToken>()))
+        _deviceClientWrapperMock.Setup(device => device.GetBlobUri(It.IsAny<FileUploadSasUriResponse>(), It.IsAny<CancellationToken>()))
         .ReturnsAsync(STORAGE_URI);
 
         _fileStreamerWrapperMock.Setup(f => f.CreateStream(It.IsAny<string>(), It.IsAny<FileMode>(), It.IsAny<FileAccess>(), It.IsAny<FileShare>(), It.IsAny<int>(), It.IsAny<bool>()))
