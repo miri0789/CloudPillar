445ce924212b9c59d11f31c3762763db21462157
mmarzbac <mmarzbac@its.jnj.com>
2023-12-21 15:39:05 +0200

    fix

--- src/agent/CloudPillar.Agent/Entities/DownloadSettings.cs
+++ src/agent/CloudPillar.Agent/Entities/DownloadSettings.cs
@@ -4,5 +4,5 @@ public class DownloadSettings
 {
     public int SignFileBufferSize { get; set; } = 16384;
     public int CommunicationDelaySeconds { get; set; } = 30;
-    public int BlockedDelayMinutes { get; set; } = 1;
+    public int BlockedDelayMinutes { get; set; } = 10;
 }
