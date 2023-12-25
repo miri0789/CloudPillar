f486a75c75d8d0476045282fffe55b7dffc62315
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-12-25 11:40:25 +0200

    pr

--- src/agent/CloudPillar.Agent/Wrappers/MatcherWrapper.cs
+++ src/agent/CloudPillar.Agent/Wrappers/MatcherWrapper.cs
@@ -14,12 +14,4 @@ public class MatcherWrapper : IMatcherWrapper
         matcher.AddIncludePatterns(patterns);
         return matcher.Match(rootPath.ToLower(), filePath.ToLower());
     }
-    public bool DoesFileMatchPattern(PatternMatchingResult matchingResult, string rootPath, string filePath)
-    {
-        return matchingResult?.Files.Any(file => filePath.Replace(FileConstants.DOUBLE_SEPARATOR, FileConstants.SEPARATOR)
-        .Replace(FileConstants.DOUBLE_FORWARD_SLASH_SEPARATOR, FileConstants.SEPARATOR)?.ToLower() ==
-        Path.Combine(rootPath, file.Path).Replace(FileConstants.DOUBLE_SEPARATOR, FileConstants.SEPARATOR)
-        .Replace(FileConstants.DOUBLE_FORWARD_SLASH_SEPARATOR, FileConstants.SEPARATOR)?.ToLower()) ?? false;
-    }
-
 }
