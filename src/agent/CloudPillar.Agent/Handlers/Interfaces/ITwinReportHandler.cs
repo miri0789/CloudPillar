28831a607085d5b981d35eee2d1dc405145b840f
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-12-26 11:58:38 +0200

    meger

---
+++ src/agent/CloudPillar.Agent/Handlers/Interfaces/ITwinReportHandler.cs
@@ -0,0 +1,25 @@
+using CloudPillar.Agent.Entities;
+using Shared.Entities.Twin;
+
+namespace CloudPillar.Agent.Handlers;
+
+public interface ITwinReportHandler
+{
+    Task UpdateReportedChangeSpecAsync(TwinReportedChangeSpec changeSpec, TwinPatchChangeSpec changeSpecKey, CancellationToken cancellationToken);
+
+    Task UpdateReportActionAsync(IEnumerable<ActionToReport> actionsToReported, CancellationToken cancellationToken);
+
+    Task UpdateDeviceStateAsync(DeviceStateType deviceState, CancellationToken cancellationToken);
+    Task<DeviceStateType?> GetDeviceStateAsync(CancellationToken cancellationToken = default);
+
+    Task UpdateDeviceStateAfterServiceRestartAsync(DeviceStateType? deviceState, CancellationToken cancellationToken);
+    Task<DeviceStateType?> GetDeviceStateAfterServiceRestartAsync(CancellationToken cancellationToken = default);
+
+    Task UpdateDeviceSecretKeyAsync(string secretKey, CancellationToken cancellationToken);
+
+    Task UpdateDeviceCertificateValidity(int CertificateExpiredDays, CancellationToken cancellationToken);
+
+    Task InitReportDeviceParamsAsync(CancellationToken cancellationToken);
+
+    Task UpdateDeviceCustomPropsAsync(List<TwinReportedCustomProp> customProps, CancellationToken cancellationToken = default);
+}
