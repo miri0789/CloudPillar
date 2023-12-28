e5ae603850ba36a1697d6c6f9d87e5c93b9af3dd
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2023-12-28 11:11:34 +0200

    Bug 13963: When performing actions in a device (download and upload) - ERROR- OnDesiredPropertiesUpdate failed message: Object reference not set to an instance of an object.

--- src/agent/CloudPillar.Agent/Handlers/TwinHandler.cs
+++ src/agent/CloudPillar.Agent/Handlers/TwinHandler.cs
@@ -120,7 +120,7 @@ public class TwinHandler : ITwinHandler
     {
         var twinDesiredChangeSpec = twinDesired.GetDesiredChangeSpecByKey(changeSpecKey);
         var twinReportedChangeSpec = twinReported.GetReportedChangeSpecByKey(changeSpecKey);
-        if (twinDesiredChangeSpec.Id != twinReportedChangeSpec.Id)
+        if (twinDesiredChangeSpec?.Id != twinReportedChangeSpec?.Id)
         {
             CancelCancellationToken();
             _twinCancellationTokenSource = new CancellationTokenSource();
