88389457cbf37e96c1f53730d63586e1063c676b
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2024-01-01 10:28:43 +0200

    comments

--- src/agent/CloudPillar.Agent/Handlers/StreamingFileUploaderHandler.cs
+++ src/agent/CloudPillar.Agent/Handlers/StreamingFileUploaderHandler.cs
@@ -89,7 +89,7 @@ public class StreamingFileUploaderHandler : IStreamingFileUploaderHandler
             await _d2CMessengerHandler.SendStreamingUploadChunkEventAsync(buffer, storageUri, currentPosition, checkSum, cancellationToken, isRunDiagnostics);
             if (!actionToReport.UploadCompleted)
             {
-                Task.Delay(1000).Wait();
+                await Task.Delay(1000);
                 await CompleteFileUploadAsync(notification, actionToReport, cancellationToken);
             }
             var percents = CalculateByteUploadedPercent(readStream.Length, currentPosition, buffer.Length);
