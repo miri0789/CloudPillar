12284e27eebbe343f057d78aa440f3bdfcf2adbe
Yael Ackerman <yakerman@its.jnj.com>
2024-02-05 11:55:14 +0200

    Assign ChangeSpec Tests

--- src/backend/UnitTests/Common.Tests/TwinDiseredServiceTestFixture.cs
+++ src/backend/UnitTests/Common.Tests/TwinDiseredServiceTestFixture.cs
@@ -7,6 +7,8 @@ using Shared.Entities.Twin;
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.Devices.Shared;
 using Shared.Entities.Utilities;
+using System.Text;
+using Newtonsoft.Json;
 
 
 namespace Backend.Infra.Common.tests;
@@ -15,21 +17,25 @@ public class TwinDiseredServiceTestFixture
     private ITwinDiseredService _target;
     private Mock<IRegistryManagerWrapper> _registryManagerWrapperMock;
     private Mock<ILoggerHandler> _loggerHandlerMock;
-    
+    private object assignChangeSpec;
+    private TwinDesired desired;
+    private TwinDesired desiredChangeSpec;
+
     [SetUp]
     public void SetUp()
     {
         _registryManagerWrapperMock = new Mock<IRegistryManagerWrapper>();
         _loggerHandlerMock = new Mock<ILoggerHandler>();
+        assignChangeSpec = "{\"id\":\"try\",\"patch\":{\"transist2\":[{\"source\":\"tryDownloadShort.txt\",\"destinationPath\":\"C:/cp/tryDownloadShort.txt\",\"unzip\":false,\"action\":2,\"sign\":\"ALXnXdOQOIwYKBQavYWRnF0XHOJVtQRyZxkJo1ViGEFiWb/01cVMXcrUmt4QSfjn7kINGUxYbK+L/lIw6KJ/xIXzAOG3JouHgbxFHT72pW3CjTc0RFMYZfjqLuTBGyFfCkX0Pb2xuqHuYXr6qPA2tUmLwBc4jDOeRcRMdeMrsIFHfuNh\",\"description\":\"deny\"}]}}";
+        SetupDesireds();
 
         _registryManagerWrapperMock.Setup(x => x.CreateFromConnectionString()).Returns(new RegistryManager());
         _target = new TwinDiseredService(_loggerHandlerMock.Object, _registryManagerWrapperMock.Object);
     }
 
-    [Test]
-    public async Task AddDesiredRecipeAsync_NoChangeSpecId_SetId()
+    private void SetupDesireds()
     {
-        var desired = new TwinDesired()
+        desired = new TwinDesired()
         {
             ChangeSpec = new Dictionary<string, TwinChangeSpec>()
             {
@@ -41,7 +47,23 @@ public class TwinDiseredServiceTestFixture
             }
         };
 
+        desiredChangeSpec = new TwinDesired()
+        {
+            ChangeSpec = new Dictionary<string, TwinChangeSpec>()
+            {
+                {
+                TwinConstants.CHANGE_SPEC_NAME, new TwinChangeSpec() {
+                        Id = "try",
+                        Patch = new Dictionary<string, TwinAction[]>()
+                    }
+                }
+            }
+        };
+    }
 
+    [Test]
+    public async Task AddDesiredRecipeAsync_NoChangeSpecId_SetId()
+    {
         var reported = new TwinReported();
         var twin = MockHelperBackend.CreateTwinMock(desired.ChangeSpec, reported.ChangeSpec);
         _registryManagerWrapperMock.Setup(x => x.GetTwinAsync(It.IsAny<RegistryManager>(), It.IsAny<string>())).ReturnsAsync(twin);
@@ -52,5 +74,70 @@ public class TwinDiseredServiceTestFixture
         _registryManagerWrapperMock.Verify(x => x.UpdateTwinAsync(It.IsAny<RegistryManager>(), It.IsAny<string>(),
          It.Is<Twin>(c => twinDesired.GetDesiredChangeSpecByKey(TwinConstants.CHANGE_SPEC_NAME).Id.Contains(TwinConstants.CHANGE_SPEC_NAME.ToString())), It.IsAny<string>()), Times.Once);
     }
-}
 
+    [Test]
+    public async Task AddChangeSpec_NoChangeSpecKey_AddChangeSpec()
+    {
+        var reported = new TwinReported();
+        var twin = MockHelperBackend.CreateTwinMock(desired.ChangeSpec, reported.ChangeSpec);
+        _registryManagerWrapperMock.Setup(x => x.GetTwinAsync(It.IsAny<RegistryManager>(), It.IsAny<string>())).ReturnsAsync(twin);
+
+        var twinDesired = await _target.AddChangeSpec("deviceId", TwinConstants.CHANGE_SPEC_NAME, assignChangeSpec);
+        Assert.AreEqual(twinDesired.ChangeSpec.Count(), 1);
+    }
+
+    [Test]
+    public async Task AddChangeSpec_OnChangeSpecKeyExists_AddChangeSpec()
+    {
+
+        var reported = new TwinReported();
+        var twin = MockHelperBackend.CreateTwinMock(desiredChangeSpec.ChangeSpec, reported.ChangeSpec);
+        _registryManagerWrapperMock.Setup(x => x.GetTwinAsync(It.IsAny<RegistryManager>(), It.IsAny<string>())).ReturnsAsync(twin);
+
+        var twinDesired = await _target.AddChangeSpec("deviceId", TwinConstants.CHANGE_SPEC_NAME, assignChangeSpec);
+        Assert.AreEqual(twinDesired.ChangeSpec.Count(), 1);
+    }
+
+    [Test]
+    public async Task GetTwinDesiredDataToSign_OnCall_ReturnBytse()
+    {
+        var changeSpec = desiredChangeSpec.GetDesiredChangeSpecByKey(TwinConstants.CHANGE_SPEC_NAME);
+        var dataToSign = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(changeSpec));
+        Assert.AreEqual(_target.GetTwinDesiredDataToSign(desiredChangeSpec, TwinConstants.CHANGE_SPEC_NAME), dataToSign);
+    }
+
+    [Test]
+    public async Task GetTwinDesiredAsync_OnCall_ReturnTwinDesired()
+    {
+        var reported = new TwinReported();
+        var twin = MockHelperBackend.CreateTwinMock(desiredChangeSpec.ChangeSpec, reported.ChangeSpec);
+        _registryManagerWrapperMock.Setup(x => x.GetTwinAsync(It.IsAny<RegistryManager>(), It.IsAny<string>())).ReturnsAsync(twin);
+
+        var twinDesired = await _target.GetTwinDesiredAsync("deviceId");
+        Assert.AreEqual(twinDesired.ChangeSpec.Count(), 1);
+    }
+
+    [Test]
+    public async Task GetTwinDesiredAsync_OnNoDeviceId_ThrowExeption()
+    {
+        Assert.ThrowsAsync<ArgumentNullException>(async () => await _target.GetTwinDesiredAsync(null));
+    }
+
+    [Test]
+    public async Task GetTwinDesiredAsync_OnNoTwinCall_ThrowExeption()
+    {
+        var res = await _target.GetTwinDesiredAsync("deviceId");
+        Assert.IsNull(res);
+    }
+
+    [Test]
+    public async Task SignTwinDesiredAsync_OnCall_ReturnTwinDesired()
+    {
+        var reported = new TwinReported();
+        var twin = MockHelperBackend.CreateTwinMock(desiredChangeSpec.ChangeSpec, reported.ChangeSpec);
+        _registryManagerWrapperMock.Setup(x => x.GetTwinAsync(It.IsAny<RegistryManager>(), It.IsAny<string>())).ReturnsAsync(twin);
+
+        await _target.SignTwinDesiredAsync(desiredChangeSpec, "deviceId", TwinConstants.CHANGE_SPEC_NAME, "signature");
+        _registryManagerWrapperMock.Verify(x => x.UpdateTwinAsync(It.IsAny<RegistryManager>(), It.IsAny<string>(), It.IsAny<Twin>(), It.IsAny<string>()), Times.Once);
+    }
+}
--- src/backend/UnitTests/Keyholder.Tests/SigningTestFixture.cs
+++ src/backend/UnitTests/Keyholder.Tests/SigningTestFixture.cs
@@ -48,47 +48,4 @@ public class SigningTestFixture
         string pemContent = @"LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tCk1JSHVBZ0VBTUJBR0J5cUdTTTQ5QWdFR0JTdUJCQUFqQklIV01JSFRBZ0VCQkVJQml5QWE3YVJIRkRDaDJxZ2EKOXNUVUdJTkU1akhBRm5tTTh4V2VUL3VuaTVJNHROcWhWNVh4MHBEcm1DVjltYnJvRnRmRWEwWFZmS3VNQXh4ZgpaNkxNL3lLaGdZa0RnWVlBQkFHQnpnZG5QNzk4RnNMdVdZVEREUUE3YzByM0JWazhOblJVU2V4cFFVc1JpbFBOCnYzU2NoTzBsUnc5UnU4Nngxa2huVkR4K2R1cTRCaURGY3ZsU0FjeWpMQUNKdmp2b3lUTEppQStUUUZkbXJlYXIKak1pWk5FMjVwVDJ5V1AxTlVuZEp4UGN2VnRmQlc0OGtQT212a1k0V2xxUDViQXdDWHdic0tyQ2drNnhic3AxMgpldz09Ci0tLS0tRU5EIFBSSVZBVEUgS0VZLS0tLS0=";
         _mockEnvironmentsWrapper.Setup(c => c.signingPem).Returns(pemContent);
     }
-
-    [Ignore("Move tests to beapi")]
-    [Test]
-    public async Task CreateTwinKeySignature_ValidArguments_SignatureAddedToTwin()
-    {
-        var deviceId = "testDevice";
-        var keyPath = "path";
-
-        var twin = new Twin();
-        twin.Properties.Desired = new TwinCollection();
-        twin.Properties.Desired[keyPath] = "value";
-
-        var twinJson = JObject.FromObject(twin.Properties.Desired);
-        var keyElement = twinJson.SelectToken(keyPath)!;
-
-        var dataToSign = Encoding.UTF8.GetBytes(keyElement.ToString());
-        var expectedSignature = _ecdsaMock.Object.SignData(dataToSign, HashAlgorithmName.SHA512);
-        var expectedSignatureString = Convert.ToBase64String(expectedSignature);
-
-        InitKeyFromEnvirementVar();
-
-        _registryManagerWrapper.Setup(mock => mock.UpdateTwinAsync(It.IsAny<RegistryManager>(), deviceId, It.IsAny<Twin>(), twin.ETag))
-                        .ReturnsAsync(twin)
-                        .Verifiable();
-
-        // await _target.CreateTwinKeySignature(deviceId, changeSignKey);
-    }
-
-    [Ignore("Move tests to beapi")]
-    [Test]
-    public async Task CreateFileKeySignature_ValidArguments_UpdateTwinWithSignature()
-    {
-        var deviceId = "testDevice";
-        var propName = "propName";
-        var actionIndex = 1;
-        var hash = new byte[] { 1, 2, 3, 4, 5 };
-        var twin = new Twin();
-        _registryManagerWrapper.Setup(mock => mock.UpdateTwinAsync(It.IsAny<RegistryManager>(), deviceId, It.IsAny<Twin>(), twin.ETag))
-                        .ReturnsAsync(twin)
-                        .Verifiable();
-        // await _target.CreateFileKeySignature(deviceId, propName, actionIndex, hash, TwinConstants.CHANGE_SPEC_NAME);
-        _registryManagerWrapper.Verify(r => r.GetTwinAsync(It.IsAny<RegistryManager>(), deviceId), Times.Once);
-    }
 }
