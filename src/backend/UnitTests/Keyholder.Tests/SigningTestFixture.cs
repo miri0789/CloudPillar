36277f83e41b752b9c5a1a25e7ab4629b657c8b8
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2024-02-07 13:44:38 +0200

    remove ignored test

--- src/backend/UnitTests/Keyholder.Tests/SigningTestFixture.cs
+++ src/backend/UnitTests/Keyholder.Tests/SigningTestFixture.cs
@@ -23,6 +23,8 @@ public class SigningTestFixture
     private Mock<IRegistryManagerWrapper> _registryManagerWrapper;
     private Mock<IFileStreamerWrapper> _fileStreamerWrapper;
     private Mock<ECDsa> _ecdsaMock;
+
+    private const string DEVICE_ID = "device-id";
     string changeSignKey;
 
     [SetUp]
@@ -36,6 +38,7 @@ public class SigningTestFixture
         _mockEnvironmentsWrapper.Setup(c => c.iothubConnectionString).Returns("HostName=szlabs-iot-hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=dMBNypodzUSWPbxTXdWaV4PxJTR3jCwehPFCQn+XJXc=");
         _mockEnvironmentsWrapper.Setup(c => c.kubernetesServiceHost).Returns("your-kubernetes-service-host");
         _mockEnvironmentsWrapper.Setup(c => c.SecretVolumeMountPath).Returns("");
+        _mockEnvironmentsWrapper.Setup(c => c.DefaultSecretVolumeMountPath).Returns("");
 
         _fileStreamerWrapper = new Mock<IFileStreamerWrapper>();
         var registryManagerMock = new Mock<RegistryManager>();
@@ -52,46 +55,11 @@ public class SigningTestFixture
                              .ReturnsAsync(pemContent);
     }
 
-    [Ignore("Move tests to beapi")]
     [Test]
-    public async Task CreateTwinKeySignature_ValidArguments_SignatureAddedToTwin()
+    public async Task GetSigningPublicKeyAsync_secretVolumeMountPathIsNull_TrowException()
     {
-        var deviceId = "testDevice";
-        var keyPath = "path";
+        _mockEnvironmentsWrapper.Setup(c => c.SecretVolumeMountPath).Returns("");
 
-        var twin = new Twin();
-        twin.Properties.Desired = new TwinCollection();
-        twin.Properties.Desired[keyPath] = "value";
-
-        var twinJson = JObject.FromObject(twin.Properties.Desired);
-        var keyElement = twinJson.SelectToken(keyPath)!;
-
-        var dataToSign = Encoding.UTF8.GetBytes(keyElement.ToString());
-        var expectedSignature = _ecdsaMock.Object.SignData(dataToSign, HashAlgorithmName.SHA512);
-        var expectedSignatureString = Convert.ToBase64String(expectedSignature);
-
-        InitKeyFromEnvirementVar();
-
-        _registryManagerWrapper.Setup(mock => mock.UpdateTwinAsync(It.IsAny<RegistryManager>(), deviceId, It.IsAny<Twin>(), twin.ETag))
-                        .ReturnsAsync(twin)
-                        .Verifiable();
-
-        // await _target.CreateTwinKeySignature(deviceId, changeSignKey);
-    }
-
-    [Ignore("Move tests to beapi")]
-    [Test]
-    public async Task CreateFileKeySignature_ValidArguments_UpdateTwinWithSignature()
-    {
-        var deviceId = "testDevice";
-        var propName = "propName";
-        var actionIndex = 1;
-        var hash = new byte[] { 1, 2, 3, 4, 5 };
-        var twin = new Twin();
-        _registryManagerWrapper.Setup(mock => mock.UpdateTwinAsync(It.IsAny<RegistryManager>(), deviceId, It.IsAny<Twin>(), twin.ETag))
-                        .ReturnsAsync(twin)
-                        .Verifiable();
-        // await _target.CreateFileKeySignature(deviceId, propName, actionIndex, hash, TwinConstants.CHANGE_SPEC_NAME);
-        _registryManagerWrapper.Verify(r => r.GetTwinAsync(It.IsAny<RegistryManager>(), deviceId), Times.Once);
+        Assert.ThrowsAsync<InvalidOperationException>(() => _target.GetSigningPublicKeyAsync(),"default cert secret path must be set.");
     }
 }
