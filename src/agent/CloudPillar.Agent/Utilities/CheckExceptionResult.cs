17e59380c04b61e89a723abf51bdbcba17e6e604
Yael Ackerman <yakerman@its.jnj.com>
2024-02-18 16:42:49 +0200

    Fix Bugs

--- src/agent/CloudPillar.Agent/Utilities/CheckExceptionResult.cs
+++ src/agent/CloudPillar.Agent/Utilities/CheckExceptionResult.cs
@@ -13,7 +13,7 @@ public class CheckExceptionResult : ICheckExceptionResult
         {
             var exceptionData = JObject.Parse(ex.Message);
             var error = exceptionData?["errorCode"]?.ToString();
-            return error is not null && Enum.TryParse(error, out DeviceConnectionResult errorCode) && errorCode == DeviceConnectionResult.IotHubUnauthorizedAccess ? errorCode : null;
+            return error is not null && Enum.TryParse(error, out DeviceConnectionResult errorCode) ? errorCode : null;
         }
         catch (JsonReaderException exception)
         {
