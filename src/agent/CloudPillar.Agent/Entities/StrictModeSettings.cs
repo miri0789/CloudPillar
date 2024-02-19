aec1bc3bd719323ae169336381bbf9170f491e09
mmarzbac <mmarzbac@its.jnj.com>
2024-02-19 17:42:34 +0200

    fix

--- src/agent/CloudPillar.Agent/Entities/StrictModeSettings.cs
+++ src/agent/CloudPillar.Agent/Entities/StrictModeSettings.cs
@@ -1,7 +1,6 @@
 public class StrictModeSettings
 {
     public bool StrictMode { get; set; }
-    public bool AllowHTTPAPI { get; set; } = false;
     public string? ProvisionalAuthenticationMethods { get; set; }
     public string? PermanentAuthenticationMethods { get; set; }
     public List<string>? GlobalPatterns { get; set; }
