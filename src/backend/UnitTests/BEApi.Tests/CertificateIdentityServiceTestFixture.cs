3b25a223746a5565a5f043177ffbc1eca76ce691
nechama-kesler <148771060+nechama-kesler@users.noreply.github.com>
2024-02-21 09:52:43 +0200

    fix beapi ut

--- src/backend/UnitTests/BEApi.Tests/CertificateIdentityServiceTestFixture.cs
+++ src/backend/UnitTests/BEApi.Tests/CertificateIdentityServiceTestFixture.cs
@@ -5,9 +5,7 @@ using Moq;
 using Shared.Logger;
 using Shared.Entities.Twin;
 using Backend.Infra.Common.Services.Interfaces;
-using Backend.BEApi.Services.interfaces;
 using Shared.Entities.Messages;
-using System.Security.Cryptography;
 
 namespace Backend.BEApi.Tests;
 
@@ -17,13 +15,10 @@ public class CertificateIdentityServiceTestFixture
     private Mock<ILoggerHandler> _loggerMock;
     private Mock<IHttpRequestorService> _httpRequestorServiceMock;
     private Mock<ITwinDiseredService> _twinDiseredHandlerMock;
-    private Mock<ISHA256Wrapper> _sha256WrapperMock;
     private Mock<IChangeSpecService> _changeSpecServiceMock;
     private ICertificateIdentityService _target;
     private const string DEVICE_ID = "deviceId";
     private byte[] publicKey = new byte[] { 1, 2, 3, 4 };
-    private byte[] calculateHash = new byte[] { 5, 6, 7, 8 };
-    private SHA256 sha526 = SHA256.Create();
     private string cerSign = "cerSign";
 
     [SetUp]
@@ -33,15 +28,13 @@ public class CertificateIdentityServiceTestFixture
         _loggerMock = new Mock<ILoggerHandler>();
         _httpRequestorServiceMock = new Mock<IHttpRequestorService>();
         _twinDiseredHandlerMock = new Mock<ITwinDiseredService>();
-        _sha256WrapperMock = new Mock<ISHA256Wrapper>();
         _changeSpecServiceMock = new Mock<IChangeSpecService>();
 
         _httpRequestorServiceMock.Setup(x => x.SendRequest<byte[]>(It.Is<string>(u => u.Contains("Signing/GetSigningPublicKeyAsync")), HttpMethod.Get, It.IsAny<object>(), It.IsAny<CancellationToken>())).ReturnsAsync(publicKey);
-        _sha256WrapperMock.Setup(x => x.Create()).Returns(sha526);
-        _sha256WrapperMock.Setup(x => x.GetHash(sha526)).Returns(calculateHash);
         _changeSpecServiceMock.Setup(x => x.SendToSignData(It.IsAny<byte[]>(), It.IsAny<string>())).ReturnsAsync(cerSign);
 
-        _target = new CertificateIdentityService(_loggerMock.Object, _environmentsWrapperMock.Object, _httpRequestorServiceMock.Object, _twinDiseredHandlerMock.Object, _sha256WrapperMock.Object, _changeSpecServiceMock.Object);
+        _target = new CertificateIdentityService(_loggerMock.Object, _environmentsWrapperMock.Object, _httpRequestorServiceMock.Object,
+         _twinDiseredHandlerMock.Object, _changeSpecServiceMock.Object);
     }
 
     [Test]
@@ -56,7 +49,7 @@ public class CertificateIdentityServiceTestFixture
     public async Task ProcessNewSigningCertificate_ValidProcess_UploadCertificateToBlobc()
     {
         await _target.ProcessNewSigningCertificate(DEVICE_ID);
-        _httpRequestorServiceMock.Verify(x => x.SendRequest(It.Is<string>(u => u.Contains("blob/UploadStream")), HttpMethod.Post, It.Is<StreamingUploadChunkEvent>(c => c.Data == publicKey), It.IsAny<CancellationToken>()), Times.Once);
+        _httpRequestorServiceMock.Verify(x => x.SendRequest(It.Is<string>(u => u.Contains("Blob/UploadStream")), HttpMethod.Post, It.Is<StreamingUploadChunkEvent>(c => c.Data == publicKey), It.IsAny<CancellationToken>()), Times.Once);
     }
 
 
